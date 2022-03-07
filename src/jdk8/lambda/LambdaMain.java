a85426f04694d25cbbc6d601524dc96521206128
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-07 21:38:46 +0900

    람다

--- src/jdk8/lambda/LambdaMain.java
+++ src/jdk8/lambda/LambdaMain.java
@@ -7,5 +7,7 @@ public class LambdaMain {
     public static void main(String[] args) {
         List<String> stringList = Arrays.asList("a", "ab", "abc", "abcd" ,"b", "bc", "bcd");
         stringList.forEach(System.out::println);
+
+        stringList.stream().map(s -> System.out.printf("출력테스트: %s", s));
     }
 }
