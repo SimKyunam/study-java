518321507c17b878e3e1abbcf26a44e705c0cf33
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-07 21:39:07 +0900

    람다

--- src/jdk8/lambda/LambdaMain.java
+++ src/jdk8/lambda/LambdaMain.java
@@ -6,8 +6,11 @@ import java.util.List;
 public class LambdaMain {
     public static void main(String[] args) {
         List<String> stringList = Arrays.asList("a", "ab", "abc", "abcd" ,"b", "bc", "bcd");
+
+        //반복문
         stringList.forEach(System.out::println);
 
+        //map
         stringList.stream().map(s -> System.out.printf("출력테스트: %s", s));
     }
 }
