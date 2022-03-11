80a3a2c28addbbab63173ac8b2e997ac0db95946
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-11 21:10:19 +0900

    스트림 추가

--- src/jdk8/lambda/LambdaMain.java
+++ src/jdk8/lambda/LambdaMain.java
@@ -2,15 +2,24 @@ package jdk8.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LambdaMain {
     public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("a", "ab", "abc", "abcd" ,"b", "bc", "bcd");
+        List<String> stringList = Arrays.asList("a", "ab", "abc", "abcd" ,"b", "bc", "bcd", "ab", "ab");
 
         //반복문
         stringList.forEach(System.out::println);
 
         //map
-        stringList.stream().map(s -> System.out.printf("출력테스트: %s", s));
+        List<String> newStringList = stringList.stream()
+                .map(s -> s + "추가")
+                .collect(Collectors.toList());
+
+        Set<String> newStringSet = stringList.stream()
+                .filter(s -> s.contains("ab"))
+                .collect(Collectors.toSet());
+
     }
 }
