42251bf5023e9f0f8f4da3cd6dd34429ca0fddba
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-27 00:13:59 +0900

    람다 학습코드

--- src/jdk8/lambda/ForEachFunction.java
+++ src/jdk8/lambda/ForEachFunction.java
@@ -4,6 +4,10 @@ package jdk8.lambda;
 public interface ForEachFunction<T> {
     T process(T t);
 
+    default void systemPrint() {
+        System.out.println();
+    }
+
     default ForEachFunction<T> system(ForEachFunction<T> function) {
         return (T t) -> {
             System.out.println(t);
