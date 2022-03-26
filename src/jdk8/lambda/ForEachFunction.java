ed5535b94cc47e04f7f99d44d4a10b9e2de76c0c
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-27 00:13:24 +0900

    람다 학습코드

--- src/jdk8/lambda/ForEachFunction.java
+++ src/jdk8/lambda/ForEachFunction.java
@@ -2,5 +2,12 @@ package jdk8.lambda;
 
 @FunctionalInterface
 public interface ForEachFunction<T> {
-    void process(T t);
+    T process(T t);
+
+    default ForEachFunction<T> system(ForEachFunction<T> function) {
+        return (T t) -> {
+            System.out.println(t);
+            return process(function.process(t));
+        };
+    }
 }
--- src/jdk8/lambda/LambdaMain.java
+++ src/jdk8/lambda/LambdaMain.java
@@ -1,13 +1,10 @@
 package jdk8.lambda;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class LambdaMain {
     public static void main(String[] args) {
-        ForEachFunction<String> forEachFunction = System.out::println;
-        forEachFunction.process("헬로우");
+        ForEachFunction<String> function = (x) -> x;
+        function.process("헬로우");
+
+        function.system(x -> x);
     }
 }
