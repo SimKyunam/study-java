8f34c5d9d042babfe9fe3a62f890fd50765c9167
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-03-27 12:03:36 +0900

    optional 학습코드

--- src/jdk8/optional/OptionalMain.java
+++ src/jdk8/optional/OptionalMain.java
@@ -1,4 +1,15 @@
 package jdk8.optional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class OptionalMain {
+    public static void main(String[] args) {
+        User user1 = new User();
+        User user2 = new User();
+
+        List<User> userList = Arrays.asList(user1, user2);
+        Optional<List<User>> optionalUsers = Optional.of(userList);
+    }
 }
